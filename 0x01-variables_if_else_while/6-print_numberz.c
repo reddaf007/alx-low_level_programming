c6c7e3f2fbc9d66bbcc772da6ec8063e513f7b58
reddaf007 <yassinereddaf@gmail.com>
2023-09-10 12:49:14 -0700

    l

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,17 +3,18 @@
 /**
  * main -Entry point
  *
- * Descrition: print 0-9 using putchar while using int variable
+ * Description: print 0-9 using putchar while using int variable
  *
- * Return: Always 0 (success)
+ * Return: Always 0(seccess)
 */
+
 int main(void)
 {
-	int digit = 0,
+	int digit = 0;
 
 	while (digit <= 9)
 	{
-		/*convert digit to ASCII representation*/
+		/*convert digit to ASCII reprsentation*/
 		putchar(digit + '0');
 		digit++;
 	}
