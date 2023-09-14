78e3b176c05b05923cadf0c27710d5c096bffd76
reddaf007 <yassinereddaf@gmail.com>
2023-09-14 09:22:59 -0700

    h

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - returns the Product of its parameters
+ * @a: int type number
+ * @b: int type number
+ * Return:0
+ */
+
+
+int mul(int a, int b)
+{
+return (a * b);
+}
